de0e084dfcf52acc99480bfd0ffa59dfb9910288
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-07-25 15:43:42 +0200

    feat: add Docker to project, delete unused database connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { config } from 'dotenv'
 import cookieParser from 'cookie-parser'
 import express from 'express'
-import mongoose from 'mongoose'
 import cors from 'cors'
 import userAccountRouter from './routes/userAccount/spotifyAccount.routes'
 import userRoutes from './routes/userAccount/user.routes'
@@ -9,29 +8,12 @@ import userRoutes from './routes/userAccount/user.routes'
 config()
 const app = express()
 
-const mongoConnectionString = process.env.MONGO_CONNECTED_STRING
 const localhost = process.env.ORIGIN
 
 const corsOptions = {
 	origin: localhost,
 	credentials: true,
 }
-
-mongoose.connect(mongoConnectionString, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-})
-
-const database = mongoose.connection
-
-database.on('error', (error) => {
-	console.log(error)
-})
-
-database.once('connected', () => {
-	console.log('Database Connected')
-})
 
 app.set('Access-Control-Allow-Credentials', true)
 app.set('Access-Control-Allow-Origin', '*')
